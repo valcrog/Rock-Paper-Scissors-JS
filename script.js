8db4efb4098eed9bb5caebe57e9dc629591e4877
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 00:47:49 +0200

    Added playRound function

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result = "";
+    if (playerSelection == computerSelection) {
+        result = "It's a tie between "+playerSelection+" !";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            result = "You Lose! Paper beats Rock";
+        }
+        else {
+            result = "You win! Rock beats Scissors";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            result = "You win! Paper beats rock";
+        }
+        else {
+            result = "You lose! Scissors beats Paper";
+        }
+    } else {
+        if (computerSelection == "rock") {
+            result = "You lose! Rock beats Scissors";
+        } else {
+            result = "You win! Scissors beats Paper"
+        }
+    }
+    return result;
+}
 
-console.log(getComputerChoice());
-testProbas();
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
